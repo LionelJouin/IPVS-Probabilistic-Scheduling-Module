426bc414d7820520aaed549fceef978ef2a342e8
Lionel Jouin <lionel.jouin@outlook.com>
2019-10-19 22:42:06 +0200

    Implementation #3

--- ip_vs_prob.c
+++ ip_vs_prob.c
@@ -9,27 +9,60 @@
 
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/random.h>
 
 #include <net/ip_vs.h>
-
 
 /*
  * Probabilistic Scheduling Scheduling
  */
 static struct ip_vs_dest *
 ip_vs_prob_schedule(struct ip_vs_service *svc, const struct sk_buff *skb,
-		  struct ip_vs_iphdr *iph)
+					struct ip_vs_iphdr *iph)
 {
-	return NULL;
+	struct ip_vs_dest *current_dest, *dest = NULL;
+	int weights_sum = 0;
+	int random_value = 0;
+	int current_sum = 0;
+
+	IP_VS_DBG(6, "%s(): Scheduling...\n", __func__);
+
+	list_for_each_entry_rcu(current_dest, &svc->destinations, n_list)
+	{
+		weights_sum += atomic_read(&current_dest->weight);
+	}
+
+	random_value = get_random_int() % weights_sum + 1;
+
+	list_for_each_entry_rcu(current_dest, &svc->destinations, n_list)
+	{
+		current_sum += atomic_read(&current_dest->weight);
+		if (random_value <= current_sum)
+		{
+			dest = current_dest;
+			break;
+		}
+	}
+
+	if (!dest)
+		ip_vs_scheduler_err(svc, "no destination available");
+	else
+		IP_VS_DBG_BUF(6, "PROB: server %s:%u activeconns %d "
+						 "inactconns %d\n",
+					  IP_VS_DBG_ADDR(dest->af, &dest->addr),
+					  ntohs(dest->port),
+					  atomic_read(&dest->activeconns),
+					  atomic_read(&dest->inactconns));
+
+	return dest;
 }
 
-
 static struct ip_vs_scheduler ip_vs_prob_scheduler = {
-	.name =			"prob",			/* name */
-	.refcnt =		ATOMIC_INIT(0),
-	.module =		THIS_MODULE,
-	.n_list =		LIST_HEAD_INIT(ip_vs_prob_scheduler.n_list),
-	.schedule =		ip_vs_prob_schedule,
+	.name = "prob", /* name */
+	.refcnt = ATOMIC_INIT(0),
+	.module = THIS_MODULE,
+	.n_list = LIST_HEAD_INIT(ip_vs_prob_scheduler.n_list),
+	.schedule = ip_vs_prob_schedule,
 };
 
 static int __init ip_vs_prob_init(void)
